0c63aa77feef454dea430b619289191b04e66cd2
KesavCodes <kesavakumarcode@outlook.com>
2024-11-28 00:34:49 +0530

    [serving file and supporting compression]

--- HTTP-server-javascript/app/main.js
+++ HTTP-server-javascript/app/main.js
@@ -1,4 +1,8 @@
 const net = require("net");
+const fs = require("fs");
+const path = require("path");
+
+const supportedCompressions = ["gzip"];
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
@@ -10,8 +14,17 @@ const server = net.createServer((socket) => {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
     } else if (url.startsWith("/echo/")) {
       const echoStr = url.split("/")[2];
+      const compressionHeader = requestSplit.filter((item) =>
+        item.toLowerCase().includes("accept-encoding:")
+      );
+      const compressData =
+        compressionHeader.length &&
+        supportedCompressions.includes(compressionHeader[0].split(":")[1].trim());
+
       socket.write(
-        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:${echoStr.length}\r\n\r\n${echoStr}`
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n${
+          compressData ? "Content-Encoding: gzip\r\n" : ""
+        }Content-Length:${echoStr.length}\r\n\r\n${echoStr}`
       );
     } else if (url.startsWith("/user-agent")) {
       const userAgent = requestSplit.filter((item) =>
@@ -25,9 +38,26 @@ const server = net.createServer((socket) => {
           `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-length:${userAgentValue.length}\r\n\r\n${userAgentValue}`
         );
       }
+    } else if (url.startsWith("/files/")) {
+      const fileName = url.split("/")[2];
+      try {
+        const fileData = fs.readFileSync(
+          path.join(__dirname, "..", "public", fileName)
+        );
+        socket.write(
+          `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length:${fileData.byteLength}\r\n\r\n${fileData}`
+        );
+      } catch (err) {
+        if (err?.code === "ENOENT") {
+          socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        } else {
+          socket.write("HTTP/1.1 500 Server Issue\r\n\r\n");
+        }
+      }
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
+    socket.end();
   });
   socket.on("close", () => {
     console.log("User is disconnected!");
